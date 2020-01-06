6a718e95d050598271db46ec8cf6e94ae7bea411
jgonthego <jgonthego@users.noreply.github.com>
2020-01-06 17:00:45 -0500

    Update json.js

--- json.js
+++ json.js
@@ -2,9 +2,9 @@ $().ready(function(){
     $.getJSON( "/data.json", function( data ) {
 //    console.log(data);
 //    $("#text").html(data["text"]);
-      var text = `Name: ${data.string}<br>
-                  Amount: ${data.string}<br>
-                  Image: ${data.string}`
+      var text = `Name: ${data.name}<br>
+                  Amount: ${data.amount}<br>
+                  Image: ${data.image}`
                     
         
         $("#text").html(text);
