5ac28a5da79471511806489adef982ee3a1c0ed7
jgonthego <jgonthego@users.noreply.github.com>
2020-01-06 16:39:01 -0500

    Update json.js

--- json.js
+++ json.js
@@ -1,6 +1,12 @@
 $().ready(function(){
     $.getJSON( "/data.json", function( data ) {
-    console.log(data);
-    $("#text").html(data["text"]);
-  });
+//    console.log(data);
+//    $("#text").html(data["text"]);
+      var text = `Name: ${data.string}<br>
+                  Amount: ${data.string}<br>
+                  Image: ${data.string}`
+                    
+        
+        $(".text").html(text);
+    });
 });
